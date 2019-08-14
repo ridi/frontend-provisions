7c49724b5dcfbc4be4f9acf6de554bc1ea4471dd
Heejae Jang <j132134@gmail.com>
2019-08-14 19:17:54 +0900

    Fix: books_ridi_io waf

--- terraform/common/waf.tf
+++ terraform/common/waf.tf
@@ -1,54 +1,3 @@
-resource "aws_waf_ipset" "gitlab_shared_runner" {
-  name = "RIDI Gitlab Shared Runner"
-
-  ip_set_descriptors = [
-    {
-      type = "IPV4"
-      value = "222.231.4.163/32"
-    },
-    {
-      type = "IPV4"
-      value = "52.78.157.166/32"
-    },
-    {
-      type = "IPV4"
-      value = "13.125.24.99/32"
-    }
-  ]
-}
-
-
-resource "aws_waf_ipset" "office_ipset" {
-  name = "RIDI Office"
-
-  ip_set_descriptors = [
-    {
-      type  = "IPV4"
-      value = "218.232.41.2/32"
-    },
-    {
-      type  = "IPV4"
-      value = "218.232.41.3/32"
-    },
-    {
-      type  = "IPV4"
-      value = "218.232.41.4/32"
-    },
-    {
-      type  = "IPV4"
-      value = "218.232.41.5/32"
-    },
-    {
-      type  = "IPV4"
-      value = "222.231.4.164/32"
-    },
-    {
-      type  = "IPV4"
-      value = "222.231.4.165/32"
-    },
-  ]
-}
-
 resource "aws_waf_rule" "allow_ridi_office_wafrule" {
   depends_on  = ["aws_waf_ipset.office_ipset"]
   name        = "ridiOfficeWAFRule"
@@ -89,23 +38,6 @@ output "in_office_waf_acl_id" {
   value = "${join("", aws_waf_web_acl.in_office_waf_acl.*.id)}"
 }
 
-resource "aws_waf_ipset" "test_ridi_io_ipset" {
-  name = "test.ridi.io"
-
-  ip_set_descriptors = [
-    {
-      type  = "IPV4"
-      value = "52.78.20.56/32"
-    },
-  ]
-}
-
-resource "aws_waf_ipset" "books_ridi_io_allowed_ipset" {
-  name = "RIDI Office and test.ridi.io and Gitlab Shared Runners"
-
-  ip_set_descriptors = "${concat(aws_waf_ipset.office_ipset.ip_set_descriptors, aws_waf_ipset.test_ridi_io_ipset.ip_set_descriptors, aws_waf_ipset.gitlab_shared_runner)}"
-}
-
 resource "aws_waf_rule" "books_ridi_io_wafrule" {
   depends_on  = ["aws_waf_ipset.books_ridi_io_allowed_ipset"]
   name        = "booksRidiIoWAFRule"
@@ -120,8 +52,8 @@ resource "aws_waf_rule" "books_ridi_io_wafrule" {
 
 resource "aws_waf_web_acl" "books_ridi_io_waf_acl" {
   depends_on = [
-    "aws_waf_ipset.office_ipset",
-    "aws_waf_rule.allow_books_ridi_io_wafrule",
+    "aws_waf_ipset.books_ridi_io_allowed_ipset",
+    "aws_waf_rule.books_ridi_io_wafrule",
   ]
 
   name        = "booksRidiIoWebACL"
@@ -143,5 +75,5 @@ resource "aws_waf_web_acl" "books_ridi_io_waf_acl" {
 }
 
 output "books_ridi_io_waf_acl_id" {
-  value = "${join("", aws_waf_web_acl.books_ridi_io_wafrule.*.id)}"
+  value = "${join("", aws_waf_web_acl.books_ridi_io_waf_acl.*.id)}"
 }
