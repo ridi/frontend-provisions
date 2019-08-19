8ed4e7ce8f3e3a9799bbac588c917623a3188c28
Heejae Jang <j132134@gmail.com>
2019-08-19 17:50:35 +0900

    Improvement common/ipset.tf

--- terraform/common/ipset.tf
+++ terraform/common/ipset.tf
@@ -56,44 +56,24 @@ resource "aws_waf_ipset" "test_ridi_io_ipset" {
 resource "aws_waf_ipset" "books_ridi_io_allowed_ipset" {
   name = "RIDI Office and test.ridi.io and Gitlab Shared Runners"
 
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "218.232.41.2/32"
+  for (ip_set in aws_waf_ipset.office_ipset.ip_set_descriptors) {
+    ip_set_descriptors {
+      type  = "IPV4"
+      value = ip_set.value
+    }
   }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "218.232.41.3/32"
+
+  for (ip_set in aws_waf_ipset.gitlab_shared_runner.ip_set_descriptors) {
+    ip_set_descriptors {
+      type  = "IPV4"
+      value = ip_set.value
+    }
   }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "218.232.41.4/32"
-  }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "218.232.41.5/32"
-  }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "222.231.4.164/32"
-  }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "222.231.4.165/32"
-  }
-  ip_set_descriptors {
-    type = "IPV4"
-    value = "222.231.4.163/32"
-  }
-  ip_set_descriptors {
-    type = "IPV4"
-    value = "52.78.157.166/32"
-  }
-  ip_set_descriptors {
-    type = "IPV4"
-    value = "13.125.24.99/32"
-  }
-  ip_set_descriptors {
-    type  = "IPV4"
-    value = "52.78.20.56/32"
+
+  for (ip_set in aws_waf_ipset.test_ridi_io_ipset.ip_set_descriptors) {
+    ip_set_descriptors {
+      type  = "IPV4"
+      value = ip_set.value
+    }
   }
 }
