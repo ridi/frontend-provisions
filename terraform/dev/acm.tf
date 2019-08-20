fad29bda36a01e499f9ef73fd6da3ec0cc42ea19
Heejae Jang <j132134@gmail.com>
2019-08-20 12:13:32 +0900

    Fix: added *.paper.ridi.io certificate

--- terraform/dev/acm.tf
+++ terraform/dev/acm.tf
@@ -1,5 +1,11 @@
 resource "aws_acm_certificate" "ridi-io" {
-  provider          = "aws.virginia"
+  provider          = aws.virginia
   domain_name       = "*.ridi.io"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate" "paper-ridi-io" {
+  provider          = aws.virginia
+  domain_name       = "*.paper.ridi.io"
   validation_method = "DNS"
 }
