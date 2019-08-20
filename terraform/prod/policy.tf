e79512960dec6bf0db57cb6eedcb5d382d175661
Heejae Jang <j132134@gmail.com>
2019-08-20 11:40:10 +0900

    Added cloudfront for policy

--- terraform/prod/policy.tf
+++ terraform/prod/policy.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "aws_s3_bucket" "policy" {
-  bucket = "policy.ridi.com"
+  bucket = "ridi-policy-prod"
 
   website {
     index_document = "index.html"
@@ -24,5 +24,53 @@ data "aws_iam_policy_document" "policy" {
       type        = "*"
       identifiers = ["*"]
     }
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "policy" {}
+
+resource "aws_cloudfront_distribution" "policy-ridi-com" {
+  origin {
+    domain_name = "${aws_s3_bucket.policy.bucket_regional_domain_name}"
+    origin_id   = "${local.policy_s3_origin_id}"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.policy.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["policy.ridicdn.net"]
+
+  default_cache_behavior {
+    allowed_methods = ["HEAD", "GET"]
+    cached_methods  = ["HEAD", "GET"]
+    compress        = false
+
+    target_origin_id = "${local.policy_s3_origin_id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
