3f6f9d541d30b672046f1d681185dd624d8de73f
Wonwoo Choi <chwo9843@gmail.com>
2020-07-06 16:06:51 +0900

    dev: Add some subnets

--- terraform/dev/vpc.tf
+++ terraform/dev/vpc.tf
@@ -1,3 +1,10 @@
+locals {
+  subnets = {
+    "ap-northeast-2a" = "172.31.0.0/20"
+    "ap-northeast-2b" = "172.31.16.0/20"
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = "172.31.0.0/16"
 }
@@ -17,5 +24,17 @@ resource "aws_subnet" "ap-northeast-2b" {
 resource "aws_subnet" "ap-northeast-2c" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "172.31.16.0/20"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_vpc" "main-new" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_subnet" "main-new" {
+  for_each                = local.subnets
+  vpc_id                  = aws_vpc.main-new.id
+  availability_zone       = each.key
+  cidr_block              = each.value
   map_public_ip_on_launch = true
 }
