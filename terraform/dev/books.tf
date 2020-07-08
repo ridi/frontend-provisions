b6ce42494fa50958a38dbd088a2f11273ce12910
Heejae Jang <j132134@gmail.com>
2020-07-08 15:06:58 +0900

    Added aws_cloudfront_distribution.books.ridi.io

--- terraform/dev/books.tf
+++ terraform/dev/books.tf
@@ -1,10 +1,38 @@
 locals {
   books_hostname     = "books.ridi.io"
+  books_lb_origin_id = "books-lb-origin"
   books_s3_origin_id = "books-s3-origin"
 }
 
 resource "aws_s3_bucket" "books" {
   bucket = "ridi-books-dev"
+}
+
+resource "aws_s3_bucket_policy" "books" {
+  bucket = "${aws_s3_bucket.books.id}"
+  policy = "${data.aws_iam_policy_document.books.json}"
+}
+
+data "aws_iam_policy_document" "books" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.books.arn}/_next/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.books.iam_arn}"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = ["${aws_s3_bucket.books.arn}/_next/"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${aws_cloudfront_origin_access_identity.books.iam_arn}"]
+    }
+  }
 }
 
 # Load Balancer
@@ -88,4 +116,79 @@ data "aws_lambda_function" "books-development" {
 
 data "aws_lambda_function" "books-staging" {
   function_name = "books-staging-server"
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_origin_access_identity" "books" {}
+
+resource "aws_cloudfront_distribution" "books-ridi-io" {
+  origin {
+    domain_name = "${aws_lb.books.arn}"
+    origin_id   = "${local.books_lb_origin_id}"
+  }
+
+  origin {
+    domain_name = "${aws_s3_bucket.books.bucket_regional_domain_name}"
+    origin_id   = "${local.books_s3_origin_id}"
+
+    s3_origin_config {
+      origin_access_identity = "${aws_cloudfront_origin_access_identity.books.cloudfront_access_identity_path}"
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["${local.books_hostname}"]
+
+  default_cache_behavior {
+    allowed_methods = ["HEAD", "GET"]
+    cached_methods  = ["HEAD", "GET"]
+    compress        = false
+
+    target_origin_id = "${local.books_lb_origin_id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward           = "whitelist"
+        whitelisted_names = ["stage"]
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "/_next/*"
+    allowed_methods  = ["GET"]
+    cached_methods   = ["GET"]
+    target_origin_id = "${local.books_s3_origin_id}"
+    compress         = true
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = "${aws_acm_certificate.ridi-io.arn}"
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
 }
