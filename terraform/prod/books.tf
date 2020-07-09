18d1a43f5aa46262bf3289c58a1762f89c8f00f5
Heejae Jang <j132134@gmail.com>
2020-07-09 18:19:37 +0900

    Added module.prod.aws_cloudfront_distribution.books-ridicdn-net

--- terraform/prod/books.tf
+++ terraform/prod/books.tf
@@ -24,7 +24,10 @@ data "aws_iam_policy_document" "books" {
 
     principals {
       type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.books.iam_arn]
+      identifiers = [
+        aws_cloudfront_origin_access_identity.books.iam_arn,
+        aws_cloudfront_origin_access_identity.cdn.iam_arn
+      ]
     }
   }
 
@@ -34,7 +37,10 @@ data "aws_iam_policy_document" "books" {
 
     principals {
       type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.books.iam_arn]
+      identifiers = [
+        aws_cloudfront_origin_access_identity.books.iam_arn,
+        aws_cloudfront_origin_access_identity.cdn.iam_arn
+      ]
     }
   }
 }
@@ -109,6 +115,7 @@ data "aws_lambda_function" "books" {
 
 # CloudFront Distribution
 resource "aws_cloudfront_origin_access_identity" "books" {}
+resource "aws_cloudfront_origin_access_identity" "cdn" {}
 
 resource "aws_cloudfront_distribution" "books-ridibooks-com" {
   origin {
@@ -226,6 +233,55 @@ resource "aws_cloudfront_distribution" "books-ridibooks-com" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.ridibooks-com.arn
+    minimum_protocol_version = "TLSv1.2_2018"
+    ssl_support_method       = "sni-only"
+  }
+
+  depends_on = [aws_s3_bucket_policy.books]
+}
+
+resource "aws_cloudfront_distribution" "books-ridicdn-net" {
+  origin {
+    domain_name = aws_s3_bucket.books.bucket_regional_domain_name
+    origin_id   = local.books_s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.books.cloudfront_access_identity_path
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = ["books.ridicdn.net"]
+
+  default_cache_behavior {
+    allowed_methods = ["HEAD", "GET"]
+    cached_methods  = ["HEAD", "GET"]
+
+    target_origin_id = local.books_lb_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.ridicdn-net.arn
     minimum_protocol_version = "TLSv1.2_2018"
     ssl_support_method       = "sni-only"
   }
