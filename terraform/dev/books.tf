4a1069c587988f8d951c90833f29733e6dd27df3
Wonwoo Choi <chwo9843@gmail.com>
2020-07-10 15:08:54 +0900

    dev/books.tf: Sort query_string_cache_keys

--- terraform/dev/books.tf
+++ terraform/dev/books.tf
@@ -177,7 +177,7 @@ resource "aws_cloudfront_distribution" "books-ridi-io" {
 
     forwarded_values {
       query_string            = true
-      query_string_cache_keys = ["is_login", "q", "search", "adult_exclude"]
+      query_string_cache_keys = ["adult_exclude", "is_login", "q", "search"]
 
       cookies {
         forward           = "whitelist"
